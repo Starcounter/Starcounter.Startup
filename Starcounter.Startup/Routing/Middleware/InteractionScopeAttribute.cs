57f803708e588e06aae3318904f223ed949a0843
Egor <egor.krushinov@starcounter.com>
2019-03-19 10:18:35 +0300

    Problem 10: UseDbScopeAttribute was replaced by InteractionScopeAttribute.

---
+++ Starcounter.Startup/Routing/Middleware/InteractionScopeAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Starcounter.Startup.Routing.Middleware
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class InteractionScopeAttribute : Attribute
+    {
+        public enum InteractionScopeMode
+        {
+            AttachOrCreate = 0,
+            AlwaysCreate = 0x1,
+            None = 0x2
+        }
+
+        public InteractionScopeMode Value { get; set; }
+
+        public InteractionScopeAttribute(InteractionScopeMode value = InteractionScopeMode.AttachOrCreate)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ Starcounter.Startup/Routing/Middleware/InteractionScopeMiddleware .cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+namespace Starcounter.Startup.Routing.Middleware
+{
+    public class InteractionScopeMiddleware : IPageMiddleware
+    {
+        private readonly InteractionScopeAttribute.InteractionScopeMode _defaultValue;
+
+        public InteractionScopeMiddleware(InteractionScopeAttribute.InteractionScopeMode defaultValue = InteractionScopeAttribute.InteractionScopeMode.AttachOrCreate)
+        {
+            _defaultValue = defaultValue;
+        }
+
+        public Response Run(RoutingInfo routingInfo, Func<Response> next)
+        {
+            var attribute = routingInfo.SelectedPageType.GetCustomAttribute<InteractionScopeAttribute>();
+
+            return GetResponseByMode(attribute?.Value ?? _defaultValue, next);
+        }
+
+        private Response GetResponseByMode(InteractionScopeAttribute.InteractionScopeMode mode, Func<Response> next)
+        {
+            switch (mode)
+            {
+                case InteractionScopeAttribute.InteractionScopeMode.AttachOrCreate:
+                    return Db.Scope(next);
+                case InteractionScopeAttribute.InteractionScopeMode.AlwaysCreate:
+                    return new Transaction().Scope(next);
+                case InteractionScopeAttribute.InteractionScopeMode.None:
+                    return next();
+            }
+
+            return null;
+        }
+    }
+}

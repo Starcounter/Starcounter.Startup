48c7f4b33b6b75395616b62d7d6e22aa667fa7cb
Egor <egor.krushinov@starcounter.com>
2019-04-04 23:35:27 +0300

    Were added .nuspec and LICENSE.txt files, AssemblyInfo was updated.

--- Starcounter.Startup/AssemblyInfo.cs
+++ Starcounter.Startup/AssemblyInfo.cs
@@ -1,3 +1,28 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Starcounter.Startup")]
+[assembly: AssemblyDescription("Dependency injection for Starcounter 2.4")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Starcounter AB")]
+[assembly: AssemblyProduct("Starcounter.Startup")]
+[assembly: AssemblyCopyright("Copyright © 2019 Starcounter AB")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7739a363-e672-476d-b560-f9f1084dfa35")]
+
+[assembly: AssemblyVersion("0.9.1")]
+[assembly: AssemblyInformationalVersion("0.9.1")]
 
 [assembly: InternalsVisibleTo("Starcounter.Startup.Tests")]
